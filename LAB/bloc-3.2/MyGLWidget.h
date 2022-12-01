0773d5c3da3a13bb89e930ecb75d4006619d9cbd
Joan Sales <113915091+joanbros5@users.noreply.github.com>
2022-12-01 10:37:51 +0100

    Add files via upload

---
+++ LAB/bloc-3.2/MyGLWidget.h
@@ -0,0 +1,29 @@
+// MyGLWidget.h
+#include "BL3GLWidget.h"
+
+class MyGLWidget : public BL3GLWidget {
+  Q_OBJECT
+
+  public:
+    MyGLWidget(QWidget *parent=0) : BL3GLWidget(parent) {}
+    ~MyGLWidget();
+
+    virtual void initializeGL();
+    virtual void carregaShaders_nuevos();
+
+  protected:
+    virtual void mouseMoveEvent(QMouseEvent *e);
+    virtual void keyPressEvent(QKeyEvent* event);
+    virtual void iniMaterialTerra ();
+
+  private:
+    int printOglError(const char file[], int line, const char func[]);
+
+    //Posició i color del focus (Vectors i Uniforms pel vertex shader)
+    glm::vec3 posFocus;
+    glm::vec3 colFocus;
+    GLuint posFocusLoc, colFocusLoc, focusEscenaLoc;
+    
+    //Bool de mentira
+    int focusEsc;
+};
